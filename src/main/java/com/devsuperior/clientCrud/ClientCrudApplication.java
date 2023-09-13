ece5f25a10563d6ada6039ece7025f666e5a15f0
Itaira <itairassa@gmail.com>
2023-09-13 11:34:39 +0200

    Entity Client

--- src/main/java/com/devsuperior/clientCrud/ClientCrudApplication.java
+++ src/main/java/com/devsuperior/clientCrud/ClientCrudApplication.java
@@ -1,13 +1,20 @@
 package com.devsuperior.clientCrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientCrudApplication {
+public class ClientCrudApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientCrudApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("READY!");
+		
 	}
 
 }
